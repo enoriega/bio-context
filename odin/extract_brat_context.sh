ed3cc013f41ce074d4de3864fcf3467ae2cc1abe
enoriega <enrique.noriega@gmail.com>
2015-08-04 13:16:37 -0700

    Progress

---
+++ odin/extract_brat_context.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+DIR='brat_sof'
+
+# Iterate through each stand off file
+for F in $DIR/*.ann
+do
+  #Extract the text-bound id of the Context-of relations
+  IDS=$(sed -n '/ContextOf/ {s/.\+Context:\(T[0-9]\+\)/\1/p}' $F)
+  for ID in $IDS
+  do
+    LINE=$(grep "^$ID\t" $F | sort -u)
+    FILE=$(echo "$LINE" | awk '{print $2}')
+    TEXT=$(echo "$LINE" | awk -F'\t' '{print $3}')
+    echo $TEXT >> $FILE.context
+  done
+done
+
+for F in *.context
+do
+  sort -u $F > $F.txt
+  rm $F
+done
